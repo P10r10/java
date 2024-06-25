58d2b73b138d8683becd888a35c25fd2af1e953e
P10R10 <asantiago@keemail.me>
2024-06-25 23:25:10 +0100

    5, 6, 7, 8

---
+++ src/Chapter_1/Ex_6.java
@@ -0,0 +1,11 @@
+package Chapter_1;
+
+public class Ex_6 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 10 ; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
---
+++ src/Chapter_1/Ex_8.java
@@ -0,0 +1,8 @@
+package Chapter_1;
+
+public class Ex_8 {
+    public static void main(String[] args) {
+        System.out.println("perimeter = " + (2 * 6.5 * 3.14159));
+        System.out.println("area = " + (6.5 * 6.5 * 3.14159));
+    }
+}
