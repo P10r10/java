619763e1c75cd593b0d47ba4c321711fb843dfed
P10R10 <asantiago@keemail.me>
2024-06-25 21:34:55 +0100

    1, 2

---
+++ src/Chapter_1/Ex_1.java
@@ -0,0 +1,9 @@
+package Chapter_1;
+
+public class Ex_1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Learning Java Now");
+        System.out.println("Programming is fun");
+    }
+}
---
+++ src/Chapter_1/Ex_2.java
@@ -0,0 +1,9 @@
+package Chapter_1;
+
+public class Ex_2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("I love Java");
+        }
+    }
+}
