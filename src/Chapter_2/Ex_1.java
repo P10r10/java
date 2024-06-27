e398ebd18640f373b6bfaa58a529208a379a55cd
P10R10 <asantiago@keemail.me>
2024-06-27 22:53:12 +0100

    1, 2

---
+++ src/Chapter_2/Ex_1.java
@@ -0,0 +1,13 @@
+package Chapter_2;
+
+import java.util.Scanner;
+
+public class Ex_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter miles: ");
+        double miles = scanner.nextDouble();
+        System.out.printf("%.0f miles is %.1f kilometers\n", miles, miles * 1.6);
+    }
+}
---
+++ src/Chapter_2/Ex_2.java
@@ -0,0 +1,15 @@
+package Chapter_2;
+
+import java.util.Scanner;
+
+public class Ex_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter length of the sides and height of the equilateral triangle: ");
+        double length = scanner.nextDouble();
+        double area = (Math.sqrt(3) / 4) * Math.pow(length, 2);
+        System.out.println("The area is " + area);
+        System.out.println("The volume of the Triangular prism is " + (area * length));
+    }
+}
